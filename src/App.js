113c7667e2b1a075d32c1930fc2b9377451b5f0b
Juan Sebastian Quinonez mURILLO <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>
2021-06-10 17:40:02 -0500

    correciones primer pull request

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react'
-import './App.css';
-// import { Carouselph } from './components/Carousel';
-// import { SingUp } from './components/SingUp';
-// import { SingIn } from './components/SingIn';
-// import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
-// import { ModalHome } from './components/ModalHome';
-import {BrowserRouter as Router, Route, Switch}  from 'react-router-dom'
-import { Home } from './views/Home';
-import { SignUpForm } from './views/SignUpForm';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./views/Home";
+import { SignUpForm } from "./views/SignUpForm";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/signup" component={SignUpForm}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup" component={SignUpForm} />
         </Switch>
       </Router>
     </div>
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import { Carouselph } from '../components/Carousel';
-import { SingUp } from '../components/SingUp';
-import { SingIn } from '../components/SingIn';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Breadcrumb from 'react-bootstrap/Breadcrumb'
-import { ModalHome } from '../components/ModalHome';
+import React from "react";
+import { Carouselph } from "../components/Carousel";
+import { SingUp } from "../components/SingUp";
+import { SingIn } from "../components/SingIn";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { ModalHome } from "../components/ModalHome";
 
 export const Home = () => {
-    return (
-        <div className="App">
-        <Container>
-          <Row className="justify-content-right">
-            <Col ><SingUp /></Col>
-            <Col><SingIn /></Col>
-            <ModalHome />
-          </Row>
-          <Row className="justify-content-center">
-              <Col className="col-7">
-                <Breadcrumb>
-                <Breadcrumb.Item>City</Breadcrumb.Item>
-                <Breadcrumb.Item>
-                    Checkin
-                </Breadcrumb.Item>
-                <Breadcrumb.Item>Checkout</Breadcrumb.Item>
-                <Breadcrumb.Item>Guests</Breadcrumb.Item>
-                </Breadcrumb>
-              </Col>
-          </Row>
-        </Container>
-        <Carouselph />
-      </div>
-    )
-}
+  return (
+    <div className="App">
+      <Container>
+        <Row className="justify-content-right">
+          <Col>
+            <SingUp />
+          </Col>
+          <Col>
+            <SingIn />
+          </Col>
+          <ModalHome />
+        </Row>
+        <Row className="justify-content-center">
+          <Col className="col-7">
+            <Breadcrumb>
+              <Breadcrumb.Item>City</Breadcrumb.Item>
+              <Breadcrumb.Item>Checkin</Breadcrumb.Item>
+              <Breadcrumb.Item>Checkout</Breadcrumb.Item>
+              <Breadcrumb.Item>Guests</Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+        </Row>
+      </Container>
+      <Carouselph />
+    </div>
+  );
+};
