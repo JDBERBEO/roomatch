e63c915d0c52cc17ce7af8e8d72557658543759d
Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>
2021-06-15 11:39:31 -0500

    filtros sin funcionalidad, de view advertisements

--- src/views/Advertisements.js
+++ src/views/Advertisements.js
@@ -1,35 +1,74 @@
 import React from "react";
 import { Carouselph } from "../components/Carousel";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
-const imgAdds = [{
-
-  title: 1,
-  src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg8.jpg", 
-  alt: "First slide", 
-  price: 0,
-  ranking: 0 
-},
-{
-  title: 2,
-  src: "https://i.pinimg.com/736x/0b/06/98/0b069802ce3cf953919291daaa80389a.jpg", 
-  alt: "Second slide", 
-  price: 0,
-  ranking: 0 
-},
-{
-  title: 3,
-  src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg", 
-  alt: "Third slide",
-  price: 0,
-  ranking: 0  
-},
-]
+const imgAdds = [
+  {
+    title: 1,
+    src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg8.jpg",
+    alt: "First slide",
+    price: 0,
+    ranking: 0,
+  },
+  {
+    title: 2,
+    src: "https://i.pinimg.com/736x/0b/06/98/0b069802ce3cf953919291daaa80389a.jpg",
+    alt: "Second slide",
+    price: 0,
+    ranking: 0,
+  },
+  {
+    title: 3,
+    src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg",
+    alt: "Third slide",
+    price: 0,
+    ranking: 0,
+  },
+];
 
 export const Advertisements = () => {
-
   return (
     <div>
-     <Carouselph array={imgAdds} />
+      <Container>
+        <Row className="justify-content-center">
+          <Col className="col-7">
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>price</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Type of accommodation</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Free cancellation</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Reserve now</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>More filters</Button>
+                </Link>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+        </Row>
+      </Container>
+
+      <Carouselph array={imgAdds} />
     </div>
   );
 };
