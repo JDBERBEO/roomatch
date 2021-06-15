63f312f6b4d94469c3e5e8a186e7d8afa860eaa8
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2021-06-15 11:10:08 -0500

    Carousel and view posts (#2)
    * home-carrusel_botones_singIn_SingUp_ no_ tiene_ los_ links
    
    * Modal de Sign In
    
    * rutas y links a signup y vista formulario
    
    * correciones primer pull request
    
    * create button that links home to advertisements
    
    * datos mokeados carrusel de imagenes en advertisements.js
    
    * intento de carousel fail por deficiencia cognitiva
    
    * reutiliza carousel de home
    
    Co-authored-by: LuisAaronHernandezRangel <lahr.4444@gmail.com>
    Co-authored-by: Juan <jd.berbeo@gmail.com>
    Co-authored-by: Juan Sebastian Quinonez mURILLO <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>

---
+++ src/components/BeHost.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export const BeHost = () => {
+  return (
+    <div>
+      <Button variant="primary">Be Host</Button>{" "}
+    </div>
+  );
+};
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,44 @@
 import React from "react";
 import { Carouselph } from "../components/Carousel";
 import { SingUp } from "../components/SingUp";
-import { SingIn } from "../components/SingIn";
+import { BeHost } from "../components/BeHost";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import { ModalHome } from "../components/ModalHome";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+
+
+const imgAddsHome = [{
+
+  title: 1,
+  src: "https://images.adsttc.com/media/images/5d34/e507/284d/d109/5600/0240/newsletter/_FI.jpg?1563747560",
+  alt: "First slide", 
+  price: 4,
+  ranking: 0 
+},
+{
+  title: 2,
+  src: "https://i.pinimg.com/originals/a6/60/9d/a6609d6a10f843c59734616accae5a89.jpg", 
+  alt: "Second slide", 
+  price: 5,
+  ranking: 0 
+},
+{
+  title: 3,
+  src: "https://soyarquitectura.mx/wp-content/uploads/2020/09/Fachada-Casa-moderna-dos-pisos-Nogal-blogc7-edited.jpg", 
+  alt: "Third slide",
+  price: 6,
+  ranking: 0  
+},
+]
+
+  
 
 export const Home = () => {
+
   return (
     <div className="App">
       <Container>
@@ -17,7 +47,7 @@ export const Home = () => {
             <SingUp />
           </Col>
           <Col>
-            <SingIn />
+            <BeHost />
           </Col>
           <ModalHome />
         </Row>
@@ -28,11 +58,16 @@ export const Home = () => {
               <Breadcrumb.Item>Checkin</Breadcrumb.Item>
               <Breadcrumb.Item>Checkout</Breadcrumb.Item>
               <Breadcrumb.Item>Guests</Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="/advertisements">
+                  <Button>Search</Button>
+                </Link>
+              </Breadcrumb.Item>
             </Breadcrumb>
           </Col>
         </Row>
       </Container>
-      <Carouselph />
+      <Carouselph array={imgAddsHome}/>
     </div>
   );
 };
