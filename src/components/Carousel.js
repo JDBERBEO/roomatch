42bc780f17d59f8efe4ce1ee04d9ed25c2792283
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2021-06-16 16:12:05 -0500

    Contenido publi (#4)
    * filtros sin funcionalidad, de view advertisements
    
    * datos mock
    
    * importa datos mockeados
    
    * crea listas de info y un solo carrusel
    
    * crea lista de información junto con un carrusel por cada lista
    
    * Componente Addlist usa componente Carruselph
    
    * correciones pull request adslist y key a la lista
    
    Co-authored-by: Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>
    Co-authored-by: kellycora <kellyc@evollve.co>

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,23 +1,18 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-export const Carouselph = ({array}) => {
-
+export const Carouselph = ({ array }) => {
   return (
     <div>
-      <Carousel> 
-        {array.map(el => (
-          <Carousel.Item key={el.title} > 
-            <img
-              className="d-block w-100"
-              src={el.src}
-              alt="First slide"
-            />
+      <Carousel>
+        {array.map((el) => (
+          <Carousel.Item key={el.title}>
+            <img className="d-block w-100" src={el.src} alt="First slide" />
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
-          </Carousel.Item> 
+          </Carousel.Item>
         ))}
       </Carousel>
     </div>
--- src/views/Advertisements.js
+++ src/views/Advertisements.js
@@ -1,35 +1,87 @@
 import React from "react";
-import { Carouselph } from "../components/Carousel";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Breadcrumb from "react-bootstrap/Breadcrumb";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import addsInfo from "../Mock_data/ads_info.json";
+import {AdsLists } from "../components/AdsLists";
 
-const imgAdds = [{
-
-  title: 1,
-  src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg8.jpg", 
-  alt: "First slide", 
-  price: 0,
-  ranking: 0 
-},
-{
-  title: 2,
-  src: "https://i.pinimg.com/736x/0b/06/98/0b069802ce3cf953919291daaa80389a.jpg", 
-  alt: "Second slide", 
-  price: 0,
-  ranking: 0 
-},
-{
-  title: 3,
-  src: "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg", 
-  alt: "Third slide",
-  price: 0,
-  ranking: 0  
-},
-]
+const imgAdds = [
+  {
+    title: 1,
+    src:
+      "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg8.jpg",
+    alt: "First slide",
+    price: 0,
+    ranking: 0,
+  },
+  {
+    title: 2,
+    src:
+      "https://i.pinimg.com/736x/0b/06/98/0b069802ce3cf953919291daaa80389a.jpg",
+    alt: "Second slide",
+    price: 0,
+    ranking: 0,
+  },
+  {
+    title: 3,
+    src:
+      "https://1.bp.blogspot.com/-ze4docdTF8g/U9iqo9uThSI/AAAAAAAABG0/du260ewvw10/s1600/Imagen8.jpg",
+    alt: "Third slide",
+    price: 0,
+    ranking: 0,
+  },
+];
 
 export const Advertisements = () => {
-
   return (
     <div>
-     <Carouselph array={imgAdds} />
+      <Container>
+        <Row className="justify-content-center">
+          <Col className="col-7">
+            <Breadcrumb>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>price</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Type of accommodation</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Free cancellation</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>Reserve now</Button>
+                </Link>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <Link to="">
+                  <Button>More filters</Button>
+                </Link>
+              </Breadcrumb.Item>
+            </Breadcrumb>
+          </Col>
+        </Row>
+
+        {addsInfo.map((el) => (
+          <AdsLists
+            key={el.id}
+            id={el.id}
+            price={el.price}
+            living_space_type={el.living_space_type}
+            description={el.description}
+            array={imgAdds}
+          />
+        ))}
+      </Container>
     </div>
   );
 };
