58675c090756535efdcbebb469bb028c7c5b5115
kellycora <kellyc@evollve.co>
2021-07-02 15:22:39 -0500

    se crea el perfil del usuario roomie

--- src/views/RoomieProfile.js
+++ src/views/RoomieProfile.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { CardProfile } from '../components/CardProfile'
+
 export const RoomieProfile = () => {
     return (
       <div>
-        <h3>Roomie Profile</h3>
+         <h3>Roomie Profile</h3>
+        <CardProfile/>
       </div>
     );
   };
