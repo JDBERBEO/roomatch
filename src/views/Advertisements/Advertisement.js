268ae45b3796ba34aad2d834bbabcdb53dfc2c36
Juan David <juan@MacBook-Air-de-Juan.local>
2021-07-04 21:51:53 -0500

    renderiza una publicación

--- src/views/Advertisements/Advertisement.js
+++ src/views/Advertisements/Advertisement.js
@@ -4,12 +4,28 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { Breadcrumb } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Breadcrumb,ListGroup } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export const Advertisement = () => {
 
- 
+  let { id } = useParams();
+  // console.log('id desde ad: ', id)
+
+  const {ads} = useSelector(({getAdsReducer})=> {
+    console.log('getreducer_ ', getAdsReducer)
+    return {
+      ads: getAdsReducer.ads
+    }
+  })
+
+  console.log(ads)
+  const adobj = ads.filter(ad => ad._id === id);
+  // console.log('ad: ', ad[0].photo)
+  // [adobj] = ad
+  
+  console.log('ad photo', adobj[0].photo)
 
   return (
     <div>
@@ -46,8 +62,17 @@ export const Advertisement = () => {
           </Col>
         </Row>
         <Row>
-          <Col className="col-7">{/* <Carouselph /> */}</Col>
-          <Col className="col-4">
+          <Col className="col-6">
+            <Carouselph array={adobj[0].photo}/>
+          </Col>
+          <Col className="col-6">
+          <ListGroup as="ul" key={adobj[0]._id}>
+              <ListGroup.Item as="li" active>
+             fferfe
+              </ListGroup.Item>
+              <ListGroup.Item as="li">{adobj[0].price}</ListGroup.Item>
+              <ListGroup.Item as="li">{adobj[0].description}</ListGroup.Item>
+            </ListGroup> 
             <Button>Book Now!</Button>
           </Col>
         </Row>
