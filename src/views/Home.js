4916d9ed3cc9fa132ad22530f1c4317d826d266e
Juan <jd.berbeo@gmail.com>
2021-07-17 17:04:57 -0500

    crea formulario de búsqueda en el home

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,29 +1,81 @@
 import React from "react";
 import { Carouselph } from "../components/Carousel";
-import { Row, Col, Container, Button, Breadcrumb } from "react-bootstrap";
+import { Row, Col, Container, Button, Card, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { imgAdds } from "../Mock_data/imgsAdd";
 import { NavBar } from "../components/NavBar";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 
 export const Home = () => {
   return (
     <div className="App">
       <NavBar />
       <Container>
-        <Row className="justify-content-right"></Row>
         <Row className="justify-content-center">
-          <Col className="col-7">
-            <Breadcrumb>
-              <Breadcrumb.Item>City</Breadcrumb.Item>
-              <Breadcrumb.Item>Checkin</Breadcrumb.Item>
-              <Breadcrumb.Item>Checkout</Breadcrumb.Item>
-              <Breadcrumb.Item>Guests</Breadcrumb.Item>
-              <Breadcrumb.Item>
-                <Link to="/advertisements">
-                  <Button>Search</Button>
-                </Link>
-              </Breadcrumb.Item>
-            </Breadcrumb>
+          <Col>
+            <Card className="text-center">
+              <Card.Header>
+                Search for your favorite living space here!
+              </Card.Header>
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <Form>
+                      <Form.Group controlId="city">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter the city"
+                          // name="public_services"
+                          // onChange={}
+                          // value={public_services}
+                        />
+                      </Form.Group>
+                    </Form>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="checkin">
+                      <Form.Label>Checkin</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter your checkin date"
+                        // name="public_services"
+                        // onChange={}
+                        // value={public_services}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="checkout">
+                      <Form.Label>City</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter your checkout date"
+                        // name="public_services"
+                        // onChange={}
+                        // value={public_services}
+                      />
+                    </Form.Group>
+                  </Col>
+                  <Col>
+                    <Form.Group controlId="guests">
+                      <Form.Label>Guests</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter the number of guests"
+                        // name="public_services"
+                        // onChange={}
+                        // value={public_services}
+                      />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Body>
+              <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
           </Col>
         </Row>
       </Container>
