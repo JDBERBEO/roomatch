02b431235a806c6c03d6fa2295bd55ed34a8c5be
LuisAaronHernandezRangel <69062150+LuisAaronHernandezRangel@users.noreply.github.com>
2021-07-27 18:21:56 -0500

    se incluye componente de pagos, response y se corrige carousel por slide (#39)

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,31 +1,36 @@
+import Carousel from "react-bootstrap/Carousel";
 import React, { useEffect } from "react";
 import M from "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
-import "materialize-css/js/parallax";
+import "materialize-css/js/carousel";
 
-export const Carouselph = () => {
+export const Carouselph = ({ array }) => {
+  let options={
+    height:400,
+    indicators: true,
+    duration: 200,
+    interval: 2000
+
+
+  };
   useEffect(() => {
-    let elements = document.querySelectorAll(".carousel");
-    M.Carousel.init(elements);
+    let elements = document.querySelectorAll(".slider");
+    M.Slider.init(elements,options);
   }, []);
-
+  console.log("array", array);
   return (
-    <div class="carousel">
-      <a class="carousel-item" href="#one!">
-        <img src="https://lorempixel.com/250/250/nature/1" alt="1" />
-      </a>
-      <a class="carousel-item" href="#two!">
-        <img src="https://lorempixel.com/250/250/nature/2" alt="2" />
-      </a>
-      <a class="carousel-item" href="#three!">
-        <img src="https://lorempixel.com/250/250/nature/3" alt="3" />
-      </a>
-      <a class="carousel-item" href="#four!">
-        <img src="https://lorempixel.com/250/250/nature/4" alt="4" />
-      </a>
-      <a class="carousel-item" href="#five!">
-        <img src="https://lorempixel.com/250/250/nature/5" alt="5" />
-      </a>
+  <div class="container">
+      <div class="slider center-align">
+        <ul class="slides">  
+        {!!array && array.length > 0 && array.map((el) => (
+          <li class="slides" key={el.title}>
+            <img class="responsive-img" src={el.src} alt="First slide" />
+          </li>
+
+        ))}
+
+        </ul>
     </div>
+  </div>
   );
 };
---
+++ src/components/Payments.js
@@ -0,0 +1,67 @@
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from 'react-router-dom'
+import axios from 'axios'
+import React from "react"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile } from "../store/profileReducer";
+
+
+
+
+export const Payments=()=> {
+  const handler = window.ePayco.checkout.configure({
+    key: process.env.REACT_APP_EPAYCO_PUBLIC_KEY,
+    test: true,
+    external: false,
+    autoclick: false,
+    lang: 'es',
+  })
+
+  function handlePayment() {
+    const data = {
+      tax: 0,
+      tax_base: 0,
+      amount: 20000,
+      name: 'Programa TOP Make it Real',
+      description: 'Programa Fullstack Inmersivo',
+
+      currency: 'cop',
+      country: 'CO',
+
+      invoice: '12345',
+      extra1: 'extra1',
+      extra2: 'extra2',
+      extra3: 'extra3',
+
+      response: `${process.env.REACT_APP_BASE_URL}/response`,
+
+      name_billing: 'María Lopez',
+      address_billing: 'Calle 23 # 54 - 10',
+      type_doc_billing: 'CC',
+      number_doc_billing: '1239075396',
+      mobilephone_billing: '3298471233',
+
+      methodsDisable: ['DP', 'PSE', 'SP'],
+    }
+    
+    handler.open(data)
+  }
+  console.log(process.env.REACT_APP_EPAYCO_PUBLIC_KEY)
+  return (
+    <div className="App">
+      <button class="waves-effect waves-pink pink btn-flat text-white" type="button" onClick={handlePayment}>Pagar
+        <i class="material-icons left">payment</i>
+      </button>
+    </div>
+  );
+}
+
+
+
+export default Payments;
