a69e688553cf73b526fcc5507049e3f8aed968ad
kellyco2019 <48420443+kellyco2019@users.noreply.github.com>
2021-07-27 16:43:44 -0500

    Validaciones front con yup y useForm Hook (#40)
    * validaciones en el front con yup
    
    * se actualizan validaciones del front
    
    * validaciones del usario roomie en el registro
    
    * se completan validaciones en el registro del roomie y del host

--- src/components/SignUpRoomie.js
+++ src/components/SignUpRoomie.js
@@ -1,104 +1,159 @@
-import  React from "react";
+import React from "react";
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { useSelector, useDispatch } from 'react-redux'
-import { register, changeName, changeLastName, changeEmail, changePassword, changeAge } from '../store/roomieReducer'
+import { registerRoomie } from '../store/roomieReducer'
 import { useHistory } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
+
+let schema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Name is required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('LastName is required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters')
+    .max(40, 'Password must not exceed 40 characters'),
+  age: Yup.number()
+    .required()
+    .positive()
+    .integer(),
+  accepTerms: Yup.bool()
+    .oneOf([true, 'Accept Terms is required'])
+})
 
 function SignUpRoomie() {
-    const dispatch = useDispatch()
-    const history = useHistory()
+  const dispatch = useDispatch()
+  const history = useHistory()
 
-    const {  loading, error, name, lastName, email, password, age } = useSelector(({ roomieReducer }) => {
-        return  {
 
-            loading: roomieReducer.loading,
-            error: roomieReducer.error,
-            name: roomieReducer.name,
-            lastName: roomieReducer.lastName,
-            email: roomieReducer.email,
-            password: roomieReducer.password,  
-            age: roomieReducer.age, 
-        } 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors } } = useForm({ resolver: yupResolver(schema) })
+
+  const {
+    loading,
+    error,
+    name,
+    lastName,
+    email,
+    password,
+    age } = useSelector(({ roomieReducer }) => {
+      return {
+
+        loading: roomieReducer.loading,
+        error: roomieReducer.error,
+        name: roomieReducer.name,
+        lastName: roomieReducer.lastName,
+        email: roomieReducer.email,
+        password: roomieReducer.password,
+        age: roomieReducer.age,
+      }
     })
-   
-    function handleSubmit(e){
-        e.preventDefault()
-        dispatch(register( name, lastName, email, password, age, history))
-   }
-    
-    if (loading) return <p>loding...</p>
-    if (error) return <p>user can not be created</p>
 
-     return(
-        <div>
-      <Container>
-        <Row className="justify-content-center">
-          <Col className="col-6">
-            <Form onSubmit={handleSubmit}> 
-              <Form.Group controlId="Name">
-                <Form.Label>Name</Form.Label>
-                <Form.Control 
-                onChange={e => dispatch(changeName(e.target.value))}
-                type="text" 
+  function createRoomie(data) {
+    dispatch(registerRoomie(
+      data.name,
+      data.lastName,
+      data.email,
+      data.password,
+      data.age,
+      history))
+    console.log(data)
+    reset();
+  }
+
+  if (loading) return <p>loding...</p>
+  if (error) return <p>user can not be created</p>
+
+  return (
+
+    <Container>
+      <Row className="justify-content-center">
+        <Col className="col-6">
+          <Form noValidate onSubmit={handleSubmit(createRoomie)}>
+            <Form.Group controlId="Name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                {...register('name')}
+                type="text"
                 placeholder="Enter name"
-                value={name}
-                name="name"
-                />
-              </Form.Group>
-              <Form.Group controlId="LastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control 
-                onChange={e => dispatch(changeLastName(e.target.value))}
-                type="text" 
+                className={`form-control ${errors.name ? 'is-invalid' : ''}`}
+                required
+              />
+              <p>{!!errors.name && errors.name.message}</p>
+            </Form.Group>
+            <Form.Group controlId="LastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                {...register('lastName')}
+                type="text"
                 placeholder="Enter last name"
-                value={lastName}
-                name="lastname"
-                />
-              </Form.Group>
-              <Form.Group controlId="Email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control 
-                onChange={e => dispatch(changeEmail(e.target.value))}
-                type="email" 
+                className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+                required
+              />
+              <p>{!!errors.lastName && errors.lastName.message}</p>
+            </Form.Group>
+            <Form.Group controlId="Email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                {...register('email')}
+                type="email"
                 placeholder="Enter Email"
-                value={email}
-                name="email"
-                 />
-              </Form.Group>
-              <Form.Group controlId="Password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control 
-                onChange={e => dispatch(changePassword(e.target.value))}
-                type="password" 
+                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+              />
+              <p>{errors.email?.message}</p>
+            </Form.Group>
+            <Form.Group controlId="Password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                {...register('password')}
+                type="password"
                 placeholder="Password"
-                value={password}
-                name="password"
-                />
-              </Form.Group>
-              <Form.Group controlId="Age">
-                <Form.Label>Age</Form.Label>
-                <Form.Control 
-                onChange={e => dispatch(changeAge(e.target.value))}
-                type="text" 
+                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+              />
+              <p>{errors.password?.message} </p>
+            </Form.Group>
+            <Form.Group controlId="Age">
+              <Form.Label>Age</Form.Label>
+              <Form.Control
+                {...register('age')}
+                type="text"
                 placeholder="Enter Age"
-                value={age} 
-                name="age"
-                />
-              </Form.Group>
-             
-               <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="accept terms" />
-              </Form.Group> 
-              <Button 
-                variant="danger"
-                type="submit">
-                Register
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-    )
+                className={`form-control ${errors.age ? 'is-invalid' : ''}`}
+              />
+              <p>{!!errors.age && errors.age.message}</p>
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="accept terms"
+                className={`form-check-input ${errors.acceptTerms ? 'is-invalid' : ''
+                  }`}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit">
+              Register
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
+
 export default SignUpRoomie
