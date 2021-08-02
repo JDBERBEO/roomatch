950a6626d5c201c3de4f5f679aabb66ad941797c
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2021-08-01 22:50:19 -0500

    renderiza info correcta en my reservations del roomie (#55)

--- src/components/ReservationsList.js
+++ src/components/ReservationsList.js
@@ -5,48 +5,81 @@ import { Carouselph } from "./Carousel";
 import { Card } from "react-bootstrap";
 
 export const ReservationsList = ({
+  reservations,
   id,
   living_space_type,
+  price,
   description,
-  paidReservation,
+  selectedDays,
   array,
+  city,
   handleSelect,
-  
 }) => {
+  console.log("reservations", reservations);
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
   return (
     <div>
       <Container>
-      <div class="divider"></div>
-      <br></br>
-      <Row>
-        <Card className="container z-depth-0" border="light">  
-          <Card.Body clasName="container">
-            <Row>
-              <Col>
-                <Card className="z-depth-5">
-                  <Card.Body>
-                    <Carouselph array={array} />
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col>
-              <Card className="text-center z-depth-5" border="light">
-                <Card.Body>
-                  <ListGroup as="ul" key={id}>
-                    <ListGroup.Item className="pink" as="li" active>{living_space_type}</ListGroup.Item>
-                    <ListGroup.Item as="li">{description}</ListGroup.Item>
-                    <ListGroup.Item as="li">{paidReservation}</ListGroup.Item>             
-                  </ListGroup>
-                  <br></br>
-                  <Button variant="danger" onClick={() => handleSelect(id)}>
-                    See reservation </Button>
-                </Card.Body>
-              </Card>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </Row>
+        <div class="divider"></div>
+        <br></br>
+        <Row>
+          <Card className="container z-depth-0" border="light">
+            <Card.Body clasName="container">
+              <Row>
+                <Col>
+                  <Card className="z-depth-5">
+                    <Card.Body>
+                      {}
+                      <Carouselph array={array} />
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card className="text-center z-depth-5" border="light">
+                    <Card.Body>
+                      <ListGroup as="ul" key={id}>
+                        <ListGroup.Item className="pink" as="li" active>
+                          <p>
+                            <strong>Living Space Name:</strong>
+                          </p>
+                          {living_space_type}
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          <p>
+                            <strong>Price:</strong>
+                          </p>
+                          {formatter.format(price)}
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          <p>
+                            <strong>Days Booked:</strong>
+                          </p>
+                          <br />
+                          {!!selectedDays &&
+                            selectedDays.length > 0 &&
+                            selectedDays.map((day) => <p>{day.toString()}</p>)}
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          <p>
+                            <strong>City:</strong>
+                          </p>
+                          {city}
+                        </ListGroup.Item>
+                      </ListGroup>
+                      <br></br>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Row>
       </Container>
     </div>
   );
