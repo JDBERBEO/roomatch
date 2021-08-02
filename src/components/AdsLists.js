5be0a4ce42e8fafc6cccad7f220f34b2a04fc164
JDBERBEO <47923495+JDBERBEO@users.noreply.github.com>
2021-08-01 23:17:00 -0500

    renderiza info correcta advertisements (#56)

--- src/components/AdsLists.js
+++ src/components/AdsLists.js
@@ -4,54 +4,80 @@ import { Col, Container, Row, Button } from "react-bootstrap";
 import { Carouselph } from "./Carousel";
 import { Card } from "react-bootstrap";
 
-
 export const AdsLists = ({
   id,
-  living_space,
+  living_space_type,
   price,
   description,
   array,
   handleSelect,
   city,
 }) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
   return (
     <div>
       <Container>
-      <div class="divider"></div>
-      <br></br>
-      <Row>
-        <Card className="container z-depth-0" border="light">  
-          <Card.Body clasName="container">
-            <Row>
-              <Col>
-                <Card className="z-depth-5">
-                  <Card.Body>
-                    <Carouselph array={array} />
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col>
-                <Card className="text-center z-depth-5" border="light">
-                  <Card.Body>
-                    <ListGroup as="ul" key={id}>
-                      <ListGroup.Item className="pink" as="li" active>
-                        {living_space}
-                      </ListGroup.Item>
-                      <ListGroup.Item as="li">{price}</ListGroup.Item>
-                      <ListGroup.Item as="li">{description}</ListGroup.Item>
-                      <ListGroup.Item as="li">{city}</ListGroup.Item>
-                    </ListGroup>
-                    <br></br>
-                    <Button variant="danger" onClick={() => handleSelect(id)}>
-                      Book this space
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </Row>
+        <div class="divider"></div>
+        <br></br>
+        <Row>
+          <Card className="container z-depth-0" border="light">
+            <Card.Body clasName="container">
+              <Row>
+                <Col>
+                  <Card className="z-depth-5">
+                    <Card.Body>
+                      <Carouselph array={array} />
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card className="text-center z-depth-5" border="light">
+                    <Card.Body>
+                      <ListGroup as="ul" key={id}>
+                        <ListGroup.Item className="pink" as="li" active>
+                          <p>
+                            <strong>
+                              Living Space Name: {living_space_type}
+                            </strong>
+                          </p>
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          <p>
+                            <strong>
+                              Price Per Day: {formatter.format(price)}
+                            </strong>
+                          </p>
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          <p>
+                            <strong>
+                              Living Space Description: {description}
+                            </strong>
+                          </p>
+                        </ListGroup.Item>
+                        <ListGroup.Item as="li">
+                          {" "}
+                          <p>
+                            <strong>City: {city}</strong>
+                          </p>
+                        </ListGroup.Item>
+                      </ListGroup>
+                      <br></br>
+                      <Button variant="danger" onClick={() => handleSelect(id)}>
+                        Book this space
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Row>
       </Container>
     </div>
   );
